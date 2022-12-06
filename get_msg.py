2b1e6c37d28a9e3d37ab14d7aad6f951895718ec
rus_is <62699147+lol-why@users.noreply.github.com>
2022-12-06 20:22:33 +0300

    Add files via upload

--- get_msg.py
+++ get_msg.py
@@ -1,6 +1,4 @@
 import imaplib
-from time import sleep
-from write_message import write_message
 from auth import email, password
 
 """
@@ -10,28 +8,25 @@ If you want to change your mail, write me
 """
 
 
-def get_msg():
+def get_msg(messages):  # messages for limit -> list with ids
     mail = imaplib.IMAP4_SSL('imap.mail.ru', 993)  # connect to mailbox
     mail.login(email, password)
     mail.list()
     mail.select("inbox")  # Receiving messages
-    messages = []  # did it for limit
-    while True:
-        result, data = mail.search(None, "ALL")
-        ids = data[0]
-        try:
-            id_list = ids.split()[-7:]
-        except IndexError:
-            id_list = ids.split()
-        if messages == id_list:
-            continue
-        messages = id_list[:]
-        print('____________________________')
-        for i in id_list:
-            result, data = mail.fetch(i, "(RFC822)")
-            raw_email = data[0][1]
-            raw_email_string = raw_email.decode('utf-8')
-            print(write_message(raw_email_string))
-        sleep(3)
+    result, data = mail.search(None, "ALL")
+    ids = data[0]
+    try:
+        id_list = ids.split()[-12:]
+    except IndexError: # if you have less, than 12 messages
+        id_list = ids.split()
+    if messages == id_list:  # check ID of messages
+        return
+    messages = id_list[:]
+    message_texts = []
+    for i in id_list:
+        result, data = mail.fetch(i, "(RFC822)")
+        raw_email = data[0][1]
+        message_texts.append(raw_email.decode('utf-8'))
+    return message_texts, messages
 
 
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from get_msg import get_msg
 from time import sleep
+from write_message import write_message
 
+messages = []  # for get_msg
 if __name__ == '__main__':
     while True:
         try:
-            get_msg()
+            request = get_msg(messages)
+            if request is None:
+                sleep(3)
+                continue
+            else:
+                print('______________________')
+                for i in request[0]:
+                    print(write_message(i))
+                messages = request[1]
+                sleep(3)
         except:
             print('______________________')
             sleep(3)
